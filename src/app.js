f3e6487c1370ea0185082980b8387a5ad4528b55
Moyra_sc <noemisolac@gmail.com>
2022-07-07 08:45:12 +0000

    exercises domain name generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".net", ".es", ".us"];
+
+  const domainNameGeneretor = (param1, param2, param3, param4) => {
+    for (let i = 0; i < param1.length; i++) {
+      for (let x = 0; x < param2.length; x++) {
+        for (let j = 0; j < param3.length; j++) {
+          for (let y = 0; y < param4.length; y++) {
+            let newNameDomain = `${param1[i]}${param2[x]}${param3[j]}${param4[y]}`;
+
+            console.log(newNameDomain);
+          }
+        }
+      }
+    }
+  };
+  domainNameGeneretor(pronoun, adj, noun, domain);
 };
